182305793219687d61a95c7d063a1392b12c355d
Dadu-deco <dadudeco@gmail.com>
2025-03-12 09:34:09 +0200

    Add project files.

---
+++ RunWithPower/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RunWithPower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Run As";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Not chosen",
+            "Invoker",
+            "Administrator",
+            "System",
+            "TrustedInstaller"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(168, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(354, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Location\\Filename";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(485, 208);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RunWithPower";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem idlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RunWithPower/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO; // Required for checking file existence
+
+namespace RunWithPower
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string arguments = $"-s -i \"{textBox1.Text}\"";
+
+            if (comboBox1.Text == "Not chosen")
+            {
+                MessageBox.Show("Please choose an option!");
+            }
+            else if (comboBox1.Text == "Invoker")
+            {
+                arguments = $"-i \"{textBox1.Text}\""; // No special privileges for Invoker
+                RunPsExec(arguments);
+            }
+            else if (comboBox1.Text == "Administrator")
+            {
+                arguments = $"-h \"{textBox1.Text}\""; // Run with Administrator privileges
+                RunPsExec(arguments);
+            }
+            else if (comboBox1.Text == "System")
+            {;
+                RunPsExec(arguments);
+            }
+            else if (comboBox1.Text == "TrustedInstaller")
+            {
+                RunPsExec(arguments);
+            }
+            else
+            {
+                MessageBox.Show("Invalid");
+            }
+        }
+
+        private void RunPsExec(string args)
+        {
+            try
+            {
+                // Check if PsExec.exe exists
+                if (!File.Exists("PsExec.exe"))
+                {
+                    MessageBox.Show("Error: PsExec.exe not found in the application folder!");
+                    return;
+                }
+
+                // Start PsExec.exe with arguments
+                Process.Start("PsExec.exe", args);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error running PsExec: {ex.Message}");
+            }
+        }
+    }
+}
